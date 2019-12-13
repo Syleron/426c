435343ae1cbdf9e8cda2554cdfb841085f0bcd30
Andrew Zak <andrew@zak.cx>
2019-12-13 15:56:45 +0000

    yes it finally works

--- client/security.go
+++ client/security.go
@@ -1,10 +1,16 @@
 package main
 
 import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
 	"crypto/sha512"
+	"encoding/base64"
 	"encoding/hex"
+	"errors"
 	"fmt"
 	gopenpgp "github.com/ProtonMail/gopenpgp/crypto"
+	"io"
 )
 
 /**
@@ -21,11 +27,12 @@ func generateKeys() {
 	hash := hashPassword("testing")
 	hashString := hex.EncodeToString(hash)
 	fmt.Println(hashString)
-	fmt.Println("Now for hash remainder...")
-	hashKey := hashString[32:]
+	fmt.Println("Now for hash key...")
+	hashKey := hashString[:32]
 	fmt.Println(hashKey)
+	fmt.Println("Now for hash remainder...")
 	// we want only 16 values
-	hashRemainder := hashKey[:16]
+	hashRemainder := hashString[32:48]
 	fmt.Println(hashRemainder)
 	// TODO: Send the remainder to the server
 	//var newHash []byte
@@ -44,22 +51,57 @@ func generateKeys() {
 	}
 	fmt.Println(rsaKey) // This is the key
 	fmt.Println("Now for encrypted private key...")
-	// encode our private key
-	//c, err := aes.NewCipher([]byte(initVector))
-	//if err != nil {
-	//	panic(err)
-	//}
-	//out := make([]byte, len(rsaKey))
-	//c.Encrypt(out, []byte(rsaKey))
-	//fmt.Println(hex.EncodeToString(out))
-	//fmt.Println("Now for descrypted private key...")
+	encryptedKey, err := encryptRSA([]byte(rsaKey), []byte(hashRemainder), []byte(hashKey))
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(encryptedKey)
+	fmt.Println("Now for decrypt private key...")
+	decryptedKey, err := decryptRSA(encryptedKey, []byte(hashRemainder), []byte(hashKey))
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(decryptedKey)
 }
 
-
-func decryptRSA() {
-
+func encryptRSA(message []byte, addData []byte, key []byte) (string, error) {
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return "", err
+	}
+	aesgcm, err := cipher.NewGCM(block)
+	if err != nil {
+		return "", err
+	}
+	nonce := make([]byte, aesgcm.NonceSize())
+	if _, err := io.ReadFull(rand.Reader, nonce); err != nil {
+		panic(err.Error())
+	}
+	ciphertext := aesgcm.Seal(nonce, nonce, message, addData)
+	return base64.URLEncoding.EncodeToString(ciphertext), nil
 }
 
-func encryptRSA() {
-
+func decryptRSA(message string, addData []byte, key []byte) (string, error) {
+	ciphertext, err := base64.URLEncoding.DecodeString(message)
+	if err != nil {
+		return "", err
+	}
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return "", err
+	}
+	aesgcm, err := cipher.NewGCM(block)
+	if err != nil {
+		return "", err
+	}
+	nonceSize := aesgcm.NonceSize()
+	if len(ciphertext) < nonceSize {
+		return "", errors.New("invalid nonce size")
+	}
+	nonce, ciphertext := ciphertext[:nonceSize], ciphertext[nonceSize:]
+	plaintext, err := aesgcm.Open(nil, nonce, ciphertext, addData)
+	if err != nil {
+		return "", err
+	}
+	return string(plaintext), nil
 }
